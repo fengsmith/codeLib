07bb6bead609d9bd702c0250776044c6ef0eaa8d
fengsmith <fengsmith1988@gmail.com>
2016-06-16 16:38:29 +0800

    commit

---
+++ src/main/java/JavaSourceFileCountStatisticsTool.java
@@ -0,0 +1,57 @@
+import java.io.File;
+
+/**
+ * author:      shfq
+ * description:
+ * create date: 2016/6/16.
+ */
+
+/**
+ * 统计一个目录下的所有 java 文件的个数（不包括测试文件）
+ */
+public class JavaSourceFileCountStatisticsTool {
+    public static void main(String[] args) {
+        String path = "C:\\Users\\Administrator\\Desktop\\netty-4.1";
+        System.out.println(recsiveStatisticsLines(new File(path)));
+    }
+
+
+    /**
+     * 统计一个文件或一个目录下的 java 文件（不包括测试文件）的个数
+     * @param file
+     * @return
+     */
+    public static int recsiveStatisticsLines(File file) {
+        if (file.isFile()) {
+            // 判断是否是 java 类但不包括测试类
+            if (isJavaFile(file) && !isJavaTestFile(file)) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        int sum = 0;
+        for (File temp : file.listFiles()) {
+            sum += recsiveStatisticsLines(temp);
+        }
+        return sum;
+    }
+
+    /**
+     * 判断一个文件是不是 java 类
+     * @param file
+     * @return
+     */
+    private static boolean isJavaFile(File file) {
+        return file.exists() && file.isFile() && file.getName().endsWith(".java");
+    }
+
+    /**
+     * 判断一个文件是不是 java 测试类
+     * @param file
+     * @return
+     */
+    private static boolean isJavaTestFile(File file) {
+        return isJavaFile(file) && file.getName().endsWith("Test.java");
+    }
+}
---
+++ src/main/java/JavaSourceFileLinesStatisticsTool.java
@@ -0,0 +1,85 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+/**
+ * author:      shfq
+ * description:
+ * create date: 2016/6/16.
+ */
+
+/**
+ * 统计一个工程下的 java 源代码的行数（不包括空行）
+ */
+public class JavaSourceFileLinesStatisticsTool {
+    public static void main(String[] args) {
+        String path = "C:\\Users\\Administrator\\Desktop\\netty-4.1";
+        System.out.println(recsiveStatisticsLines(new File(path)));
+    }
+
+    /**
+     * 统计一个文件的代码行数 但不统计空行
+     * @param file
+     * @return
+     */
+    private static int statisticsLinesOfOneFile(File file) {
+        int count = 0;
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String s;
+            while ((s = reader.readLine()) != null) {
+                // 不统计空行
+                if (s.length() != 0) {
+                    count++;
+                }
+            }
+            reader.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return count;
+    }
+
+
+    /**
+     * 统计一个文件或一个目录下的 java 文件（不包括测试文件）的代码行数
+     * @param file
+     * @return
+     */
+    public static int recsiveStatisticsLines(File file) {
+        if (file.isFile()) {
+            // 判断是否是 java 类但不包括测试类
+            if (isJavaFile(file) && !isJavaTestFile(file)) {
+                return statisticsLinesOfOneFile(file);
+            } else {
+                return 0;
+            }
+        }
+        int sum = 0;
+        for (File temp : file.listFiles()) {
+            sum += recsiveStatisticsLines(temp);
+        }
+        return sum;
+    }
+
+    /**
+     * 判断一个文件是不是 java 类
+     * @param file
+     * @return
+     */
+    private static boolean isJavaFile(File file) {
+        return file.exists() && file.isFile() && file.getName().endsWith(".java");
+    }
+
+    /**
+     * 判断一个文件是不是 java 测试类
+     * @param file
+     * @return
+     */
+    private static boolean isJavaTestFile(File file) {
+        return isJavaFile(file) && file.getName().endsWith("Test.java");
+    }
+}
