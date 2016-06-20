cc8ce4e7119acb1cba9c90ff06245fc17f201f46
fengsmith <fengsmith1988@gmail.com>
2016-06-20 11:50:34 +0800

    commit

--- src/main/java/JavaSourceFileLinesStatisticsTool.java
+++ src/main/java/JavaSourceFileLinesStatisticsTool.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 
 /**
  * author:      shfq
@@ -23,10 +24,11 @@ public class JavaSourceFileLinesStatisticsTool {
      * @return
      */
     private static int statisticsLinesOfOneFile(File file) {
+        BufferedReader reader = null;
         int count = 0;
 
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(file));
+            reader = new BufferedReader(new FileReader(file));
             String s;
             while ((s = reader.readLine()) != null) {
                 // 不统计空行
@@ -34,10 +36,18 @@ public class JavaSourceFileLinesStatisticsTool {
                     count++;
                 }
             }
-            reader.close();
-
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
         }
 
         return count;
